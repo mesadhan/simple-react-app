935ce7f35b97541117162e5ace7b85c5ac7fe042
mesadhan <sadhansarker3@gmail.com>
2018-12-15 17:36:16 +0600

    working with react map and delete function

--- src/person/Person.js
+++ src/person/Person.js
@@ -8,13 +8,12 @@ let style = {
 
 const person = (props) => {
     return (
-        <div className="Person">
+        <div className="">
 
             {/*<p style={style}>I am {props.name} and I am {Math.floor(Math.random() * 30)} years old!</p>*/}
             <p style={style} onClick={props.click}>I am <span style={{color: 'green'}}>{props.name}</span> and I am <span style={{color: 'green'}}>{props.age} </span>years old!</p>
             <div>{props.children}</div>
             <input type="text" onChange={props.changed} defaultValue={props.name}/>
-
         </div>
     )
 };
