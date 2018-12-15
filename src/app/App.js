b6467b1cf9c73df23ce3dfb04bcbdc5f07e80b1e
mesadhan <sadhansarker3@gmail.com>
2018-12-15 21:53:58 +0600

    top header component updated

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import '../components/person/Person.css'
 import Persons from "../components/person/Persons";
+import Header from "../components/header/Header";
 
 
 class App extends Component {
@@ -13,19 +14,17 @@ class App extends Component {
             {id: '3', name: 'Sadhan', age: 23}
         ],
         showPersons: true,
-        vipPerson: {id: 10, name: 'Manu', age: 29}
     };
 
 
-    clickHandler = () => {
-        // console.log('Was clicked!');
-        // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    restoreHandler = () => {
         this.setState({
             persons: [
                 {id: '1', name: 'sadhan', age: 28},
                 {id: '2', name: 'Manu', age: 29},
-                {id: '3', name: 'Stephanie', age: 27}
-            ]
+                {id: '3', name: 'Rexon', age: 27}
+            ],
+            showPersons: true,
         })
     };
 
@@ -33,8 +32,8 @@ class App extends Component {
         this.setState({
             persons: [
                 {id: '1', name: name, age: 28},
-                {id: '2', name: 'Manu', age: 29},
-                {id: '3', name: 'Stephanie', age: 27}
+                {id: '2', name: 'Manu Hossian', age: 29},
+                {id: '3', name: 'Rexon Sarker', age: 27}
             ]
         })
     };
@@ -63,11 +62,11 @@ class App extends Component {
     };
 
 
-    deletePersonHandler = ( personIndex ) => {
+    deletePersonHandler = (personIndex) => {
         // const persons = this.state.persons.slice();
         const persons = [...this.state.persons];
-        persons.splice( personIndex, 1 );
-        this.setState( { persons: persons } );
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
     };
 
 
@@ -75,30 +74,26 @@ class App extends Component {
 
         let persons = null;
 
-        if ( this.state.showPersons ) {
+        if (this.state.showPersons) {
             persons = <Persons
                 persons={this.state.persons}
                 clicked={this.deletePersonHandler}
-                changed={this.nameChangedHandler} />;
+                changed={this.nameChangedHandler}/>;
         }
-
 
 
         return (
             <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                <p>This is really working!</p>
-                <button onClick={this.clickHandler}>Click Person</button>
-                <button style={{color: 'red', fontWeight: 'bold'}} onClick={this.togglePersonHandler}>Toggle Person</button>
-                <button onClick={() => this.updateHandler('Maximilian!!')}>Update Person</button>
-
-
+                <Header
+                    clicked={this.restoreHandler}
+                    toggleClicked={this.togglePersonHandler}
+                    updateClicked={this.updateHandler}
+                />
                 {persons}
 
-                <h1>Welcome To React App</h1>
-
             </div>
-        );
+
+        )
 
         //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'))
         //return Template.call(this);
---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const header = (props) => {
+    return (
+        <div className="TopHeader">
+            <h1>Welcome To React App</h1>
+            <button style={{color: 'red', fontWeight: 'bold'}} onClick={props.toggleClicked}>Toggle Person</button>
+            <button onClick={() => props.updateClicked('Sadhan Sarker!!')}>Update Person</button>
+            <button onClick={props.clicked}>Restore Person</button>
+
+
+
+
+        </div>
+    )
+};
+
+export default header;
